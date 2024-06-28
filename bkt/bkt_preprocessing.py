9008d44d984534f4ed752e74f899a67bf9f4fdb8
inextro <88864528+inextro@users.noreply.github.com>
2024-06-28 17:52:29 +0900

    feat: implement data preprocessing for BKT model

---
+++ bkt/bkt_preprocessing.py
@@ -0,0 +1,38 @@
+import argparse
+import numpy as np
+import pandas as pd
+
+
+def extract_mc(num_topics, rating_data, flag=False, ratio=None):
+    movies = pd.read_csv('../data/crawled_data/plots_crawled.csv')
+    lda = pd.read_csv(f'../LDA/result/result_{num_topics}/20240626_Topic={num_topics}_TopicDist.csv')
+    
+    # 가장 비중이 큰 LDA 토픽을 MC로 정의
+    if flag:
+        lda['mc'] = lda.apply(lambda x: np.argmax(x.values), axis=1)
+        avg_mcs = 1
+    # 일정 비율 이상의 LDA 토픽을 MC로 정의
+    else:
+        lda['mc'] = lda.apply(lambda x: extract_mc_by_ratio(row=x, ratio=ratio), axis=1)
+        lda['n_mc'] = lda['mc'].apply(lambda x: len(x))
+        avg_mcs = lda['n_mc'].mean()
+
+    assert(movies.shape[0] == lda.shape[0])
+
+    rating_data = rating_data.merge(
+        right=lda.loc[:, ['movieId', 'mc']], 
+        how='inner', 
+        on='movieId'
+    )
+    
+    if not flag:
+        rating_data = rating_data.explode(column='mc', ignore_index=True)
+
+    return round(avg_mcs, 4), rating_data
+
+
+def extract_mc_by_ratio(row, ratio):
+    idx = row[row >= ratio].index
+    mcs = [i.split()[1] for i in idx]
+
+    return mcs
