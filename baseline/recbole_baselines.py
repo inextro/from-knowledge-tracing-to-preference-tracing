7111b84ffe11340bc6a83b6322ae67fd961e63aa
inextro <inextro@ds.seoultech.ac.kr>
2025-05-02 04:22:37 +0000

    chore: Create config.yaml for baselines

---
+++ baseline/recbole_baselines.py
@@ -0,0 +1,21 @@
+import argparse
+from recbole.quick_start import run_recbole
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-m', '--model_name', type=str, required=True)
+
+    args = parser.parse_args()
+    model_name = args.model_name
+    
+    if model_name == 'DIEN':
+        config = './configs/config_DIEN.yaml'
+    else:
+        config = './configs/configs.yaml'
+
+    run_recbole(model=model_name, dataset='ml', config_file_list=[config])
+
+
+if __name__ == '__main__':
+    main()
